a2b3d321b87e6d6e7bb2bed0df4be60041f26f3d
brandaoplaster <brandaoplaster@gmail.com>
2023-01-20 23:06:48 -0300

    feat: Creating and organizing authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
 end
