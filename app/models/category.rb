f9b948761c010cdcebaa662d094990ea29f155f1
brandaoplaster <brandaoplaster@gmail.com>
2023-01-25 15:41:47 -0300

    refactor: Changing category and tag relationship in models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :accounting_entries
-  belongs_to :tag
+  has_many :tags
 
   validates :title, presence: true
   validates :title, uniqueness: { case_sensitive: false }
